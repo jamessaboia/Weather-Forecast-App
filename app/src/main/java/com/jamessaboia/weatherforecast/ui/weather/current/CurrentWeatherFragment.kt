df17374c67518d67e0f77f7ed547a79a084fc090
James Saboia <saboiajamesjr@gmail.com>
2022-07-03 02:15:28 -0400

    adding Retrofit, Coroutines and Gson Fetch API Data settings

--- app/src/main/java/com/jamessaboia/weatherforecast/ui/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/com/jamessaboia/weatherforecast/ui/weather/current/CurrentWeatherFragment.kt
@@ -6,7 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.jamessaboia.weatherforecast.R
+import com.jamessaboia.weatherforecast.data.Api
+import com.jamessaboia.weatherforecast.databinding.CurrentWeatherFragmentBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class CurrentWeatherFragment : Fragment() {
 
@@ -14,19 +20,29 @@ class CurrentWeatherFragment : Fragment() {
         fun newInstance() = CurrentWeatherFragment()
     }
 
+    lateinit var binding: CurrentWeatherFragmentBinding
     private lateinit var viewModel: CurrentWeatherViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.current_weather_fragment, container, false)
+
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.current_weather_fragment, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(CurrentWeatherViewModel::class.java)
         // TODO: Use the ViewModel
+        val apiService = Api()
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val currentWeatherResponse = apiService.getCurrentWeather("New York").await()
+            binding.textView.text = currentWeatherResponse.toString()
+        }
     }
 
 }
