b05ef806cb6f7faf55a912e924832d825609410d
James Saboia <saboiajamesjr@gmail.com>
2022-07-04 02:18:22 -0400

    adding the current weather user interface

--- app/src/main/java/com/jamessaboia/weatherforecast/ui/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/com/jamessaboia/weatherforecast/ui/weather/current/CurrentWeatherFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.jamessaboia.weatherforecast.R
@@ -13,6 +14,7 @@ import com.jamessaboia.weatherforecast.data.network.Api
 import com.jamessaboia.weatherforecast.data.network.WeatherNetworkDataSourceImpl
 import com.jamessaboia.weatherforecast.data.network.response.ConnectivityInterceptorImpl
 import com.jamessaboia.weatherforecast.databinding.CurrentWeatherFragmentBinding
+import com.jamessaboia.weatherforecast.internal.glide.GlideApp
 import com.jamessaboia.weatherforecast.ui.base.ScopedFragment
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -55,10 +57,61 @@ class CurrentWeatherFragment : ScopedFragment(), KodeinAware {
 
     private fun bindUi() = launch{
         val currentWeather = viewModel.weather.await()
+
         currentWeather.observe(viewLifecycleOwner, Observer {
             if (it == null) return@Observer
-            binding.textView.text = it.toString()
+
+            binding.groupLoading.visibility = View.GONE
+            updateLocation("New York")
+            updateDateToToday()
+            updateTemperatures(it.temperature, it.feelslike)
+            updateCondition(it.weatherDescriptions)
+            updatePrecipitation(it.precip)
+            updateWind(it.windDir, it.windSpeed)
+            updateVisibility(it.visibility)
+
+//            GlideApp.with(this@CurrentWeatherFragment)
+//                .load(it.weatherIcons[0])
+//                .into(binding.imageViewConditionIcon)
         })
+    }
+
+    private fun chooseLocalizedUnitAbbreviation(metric: String, imperial: String): String {
+        return if (viewModel.isMetric) metric else imperial
+
+    }
+
+    private fun updateLocation(location: String) {
+        (activity as? AppCompatActivity)?.supportActionBar?.title = location
+    }
+
+    private fun updateDateToToday() {
+        (activity as? AppCompatActivity)?.supportActionBar?.subtitle = getString(R.string.today)
+    }
+
+    private fun updateTemperatures(temperature: Int, feelslike: Int) {
+        val unitAbbreviation = chooseLocalizedUnitAbbreviation("°C", "°F")
+        binding.textViewTemperature.text = "$temperature$unitAbbreviation"
+        binding.textViewFeelsLikeTemperature.text = "Feels like $feelslike$unitAbbreviation"
+    }
+
+    private fun updateCondition(condition: List<String>) {
+        binding.textViewCondition.text = condition.toString()
+    }
+
+    private fun updatePrecipitation(precipitation: Float) {
+        val unitAbbreviation = chooseLocalizedUnitAbbreviation("mm", "in")
+        binding.textViewPrecipitation.text = "Precipitation: $precipitation $unitAbbreviation"
+    }
+
+    private fun updateWind(windDirection: String, windSpeed: Int) {
+        val unitAbbreviation = chooseLocalizedUnitAbbreviation("kph", "mph")
+        binding.textViewWind.text = "Wind: $windDirection, $windSpeed $unitAbbreviation"
+    }
+
+    private fun updateVisibility(visibility: Int) {
+        val unitAbbreviation = chooseLocalizedUnitAbbreviation("km", "mi.")
+        binding.textViewVisibility.text = "Visibility: $visibility $unitAbbreviation"
     }
 
 }
